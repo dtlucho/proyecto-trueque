9c5bf3ccafc1ec0b0e8b5be405272a486efa8b4f
dtlucho <luciano.ditomaso@gmail.com>
2018-09-13 12:14:20 -0300

    Armado de algunos php

---
+++ proyecto-integrador/footer.php
@@ -0,0 +1,5 @@
+<footer class="py-3 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy;: Lucas Diaz, Santiago Bouzon y Luciano Di Tomaso.</p>
+    </div>
+</footer>
---
+++ proyecto-integrador/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-purple fixed-top">
+    <div class="container">
+        <a class="navbar-brand font-white" href="index.html">TruequeYa</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link font-white" href="index.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link font-white" href="signup.html">Registrate</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link font-white" href="login.html">Iniciar Sesión</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
